bd1269e45664b5b9e1454017ef5ccfcd57befe05
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-10 16:51:26 +0200

    [add] esercizio completo con tabella

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    public function index(){
-        return view('home');
+    public function index()
+    {
+
+        $movies = Movie::all();
+        // dd($movies);
+
+        return view('home', compact('movies'));
     }
 
-    public function nuovaPagina(){
+    public function nuovaPagina()
+    {
         return view('nuova-pagina');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,35 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>Movies</h1>
+    <div class="container my-5">
+        <h1>Movies</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">title</th>
+                    <th scope="col">original_title</th>
+                    <th scope="col">nationality</th>
+                    <th scope="col">date</th>
+                    <th scope="col">vote</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>{{ $movie->id }}</td>
+                        <td>{{ $movie->title }}</td>
+                        <td>{{ $movie->original_title }}</td>
+                        <td>{{ $movie->nationality }}</td>
+                        <td>{{ $movie->date }}</td>
+                        <td>{{ $movie->vote }}</td>
+
+                    </tr>
+                @endforeach
+
+
+
+            </tbody>
+        </table>
+    </div>
 @endsection
