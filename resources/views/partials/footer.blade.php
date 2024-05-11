6af849b955282cf58a650e7147adc49d38f6dece
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-11 23:36:52 +0200

    [add] footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,11 @@
 <footer>
-    <h1>Footer</h1>
+    {{-- <h1>Footer</h1> --}}
+    <div class="container">
+        <div class="row row-cols-4 ">
+            <div class="col">Chi siamo</div>
+            <div class="col">Help</div>
+            <div class="col">Contatti</div>
+            <div class="col">Contatti</div>
+        </div>
+    </div>
 </footer>
